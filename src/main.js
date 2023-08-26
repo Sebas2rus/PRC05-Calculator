4b2d3167568440a70f5ccdc013b7e27369a6ce14
sebas2rus <sebas2rus@gmail.com>
2023-08-25 19:56:22 -0500

    Calculator

--- src/main.js
+++ src/main.js
@@ -1,55 +1,167 @@
-import { writeData } from './writeData.js';
+import { addition, division, multiplication, substraction } from './operations.js';
 
-// Get username from input - form
+let num1 = 0;
+let num2 = 0;
+let temp = 0;
+let operationName = '';
 
+export function calc() {
 
-// Execute function to write the user info
-const form = document.getElementById('searchForm')
-export function eventHandler(e) {
-    const username = document.getElementById('search').value
-    validateData(username)
-    e.preventDefault();
-    form.reset()
-}
-form.addEventListener('submit', eventHandler)
+    switch (operationName) {
+        case 'addition':
+            num2 = document.getElementById('value').value.split('+')[1]
+            const result = addition(num1, num2)
+            document.getElementById('value').value = result;
+            break;
 
-const formi = document.getElementById('searchFormi')
-export function eventHandleri(e) {
-    const username = document.getElementById('searchi').value
-    validateData(username)
-    e.preventDefault();
-    formi.reset()
-}
-formi.addEventListener('submit', eventHandleri)
+        case 'substraction':
+            num2 = document.getElementById('value').value.split('-')[1]
 
+            const total = substraction(num1, num2)
+            document.getElementById('value').value = total;
+            break;
 
-const formii = document.getElementById('searchFormii')
-console.log(formii);
-export function eventHandlerii(e) {
-    console.log(e);
-    const username = document.getElementById('searchii').value
-    console.log(username);
-    validateData(username)
-    e.preventDefault();
-    formii.reset()
-}
-formii.addEventListener('submit', eventHandlerii)
+        case 'multiplication':
+            num2 = document.getElementById('value').value.split('*')[1]
+            const data = multiplication(num1, num2)
+            document.getElementById('value').value = data;
+            break;
 
+        case 'division':
+            num2 = document.getElementById('value').value.split('/')[1]
+            const result1 = division(num1, num2)
+            document.getElementById('value').value = result1;
+            break;
 
-
-// Defines what content to render depending on the information
-export async function validateData(username) {
-    const data = await writeData(username)
-    console.log(data);
-    if (data && data?.message == 'Not Found') {
-        const searchView = document.getElementById('searchView').style.display = 'none';
-        const foundView = document.getElementById('foundView').style.display = 'none';
-        const notFoundView = document.getElementById('notFoundView').style.display = 'block';
-
-    } else if (data && data?.login != '') {
-        const searchView = document.getElementById('searchView').style.display = 'none';
-        const notFoundView = document.getElementById('notFoundView').style.display = 'none';
-        const foundView = document.getElementById('foundView').style.display = 'block';
-
+        default:
+            num1 = document.getElementById('value').value
+            document.getElementById('value').value = num1;
+            break;
     }
+
+    document.getElementById('value').focus()
+    clearData()
 }
+
+// Here comes the operations
+
+document.getElementById('addition').addEventListener('click', () => {
+    operationName = 'addition';
+    num1 = document.getElementById('value').value
+
+    document.getElementById('value').value += ' + ';
+    document.getElementById('value').focus()
+
+})
+
+
+document.getElementById('substraction').addEventListener('click', () => {
+    operationName = 'substraction';
+    num1 = document.getElementById('value').value
+
+    document.getElementById('value').value += ' - ';
+    document.getElementById('value').focus()
+})
+
+
+document.getElementById('multiplication').addEventListener('click', () => {
+    operationName = 'multiplication';
+    num1 = document.getElementById('value').value
+
+    document.getElementById('value').value += ' * ';
+    document.getElementById('value').focus()
+
+})
+
+document.getElementById('division').addEventListener('click', () => {
+    operationName = 'division';
+    num1 = document.getElementById('value').value
+
+    document.getElementById('value').value += ' / ';
+    document.getElementById('value').focus()
+
+})
+
+const equal = document.getElementById('equal');
+equal.addEventListener('click', calc)
+
+
+// Here we can sum with buttons
+
+export function setNumber(newValue) {
+    let oldValue = document.getElementById('value').value
+    newValue = String(newValue)
+    oldValue += newValue;
+    document.getElementById('value').value = oldValue
+}
+
+for (let index = 0; index <= 9; index++) {
+    document.getElementById(index).addEventListener('click', () => setNumber(index))
+}
+
+// Here comes util actions
+
+document.body.onload = () => {
+    document.getElementById('value').focus()
+}
+
+export function clearData() {
+    num1 = 0;
+    num2 = 0;
+    operationName = '';
+    temp = 0;
+}
+
+export function clear() {
+    num1 = 0;
+    num2 = 0;
+    operationName = '';
+    temp = 0;
+    document.getElementById('value').value = '';
+    document.getElementById('value').focus();
+}
+
+document.getElementById('clear').addEventListener('click', clear)
+
+
+document.getElementById('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+})
+
+document.getElementById('form').addEventListener('keyup', (e) => {
+    var codigo = e.key;
+
+    switch (e.key) {
+        case 'Enter':
+            calc()
+            break;
+        case '+':
+            operationName = 'addition';
+            num1 = document.getElementById('value').value.split('+')[0]
+            document.getElementById('value').value = `${num1} + `;
+            document.getElementById('value').focus()
+            break;
+        case '-':
+            operationName = 'substraction';
+            num1 = document.getElementById('value').value.split('-')[0]
+            document.getElementById('value').value = `${num1} - `;
+            document.getElementById('value').focus()
+            break;
+        case '*':
+            operationName = 'multiplication';
+            num1 = document.getElementById('value').value.split('*')[0]
+            document.getElementById('value').value = `${num1} * `;
+            document.getElementById('value').focus()
+            break;
+
+        case '/':
+            operationName = 'division';
+            num1 = document.getElementById('value').value.split('/')[0]
+            document.getElementById('value').value = `${num1} / `;
+            document.getElementById('value').focus()
+            break;
+
+        default:
+            break;
+    }
+})
---
+++ src/operations.js
@@ -0,0 +1,23 @@
+export function addition(num1, num2) {
+    num1 = Number(num1)
+    num2 = Number(num2)
+    return (num1 + num2)
+}
+
+export function substraction(num1, num2) {
+    num1 = Number(num1)
+    num2 = Number(num2)
+    return (num1 - num2)
+}
+
+export function multiplication(num1, num2) {
+    num1 = Number(num1)
+    num2 = Number(num2)
+    return (num1 * num2)
+}
+
+export function division(num1, num2) {
+    num1 = Number(num1)
+    num2 = Number(num2)
+    return (num1 / num2)
+}
